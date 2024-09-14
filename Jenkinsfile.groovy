08200d3a7f7569b17373943b51476d24afd0d550
farheenasifshaikh <122972009+farheenasifshaikh@users.noreply.github.com>
2024-09-14 17:00:29 +0530

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                sh './gradlew assemble'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh './gradlew test'
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Build and tests passed!'
+        }
+        failure {
+            echo 'Build or tests failed!'
+        }
+    }
+}
